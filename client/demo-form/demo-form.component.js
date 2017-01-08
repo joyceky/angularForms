4e399ca8cec123b657d7c8ba084c15fa4f887747
Kyle Joyce <kyyjoyce@gmail.com>
2017-01-08 15:33:46 -0700

    basic form resetting and submitting

--- client/demo-form/demo-form.component.js
+++ client/demo-form/demo-form.component.js
@@ -4,7 +4,30 @@
   angular
     .module("app", [])
     .component("demoForm", {
-        templateUrl: "demo-form/demo-form.template.html"
+        templateUrl: "demo-form/demo-form.template.html",
+        controller: DemoFormController
     });
+
+  function DemoFormController () {
+
+    var vm = this;
+
+    vm.submitForm = submitForm;
+    vm.resetForm = resetForm;
+    vm.user = {};
+
+
+    function submitForm(username, password) {
+      if(username && password) {
+        console.log(username, password);
+      }
+    }
+
+    function resetForm () {
+      vm.user = {};
+      vm.usernameForm.$setPristine();
+      vm.usernameForm.$setUntouched();
+    }
+  }
 
 })();
